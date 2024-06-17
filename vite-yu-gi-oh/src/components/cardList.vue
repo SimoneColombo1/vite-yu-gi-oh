84bcef770cc8914980cd5d2d9b05b42cde515c64
SimoneColombo1 <simcolombo2@gmail.com>
2024-06-17 20:25:00 +0200

    richiamata l'api

---
+++ vite-yu-gi-oh/src/components/cardList.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+    data() {
+        return {
+            
+        }
+
+    },
+   props
+}
+</script>
+<template>
+<div class="card-container">
+    <div class="single card" v-for="(card, index) in cards" :key="index">
+       
+         </div>
+         
+    </div>
+
+</template>
+<style>
+</style>
